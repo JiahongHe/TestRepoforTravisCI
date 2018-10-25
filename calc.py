503dc63859289ad39a6514b0211a22e31bff5a5c
Jiahong He <jh3863@columbia.edu>
2018-10-25 03:06:52 -0400

    add travis staff

---
+++ calc.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class calculator:
+
+    def __init__(self):
+        self.name = "calculator"
+
+    def add(self, a, b):
+        return np.add(a, b)
+
+    def multiply(self, a, b):
+        return np.multiply(a, b)
+
+    def division(self, a, b):
+        if b != 0: 
+            return a / b
+        else:
+            raise ValueError("division by 0!")
---
+++ test_calc.py
@@ -0,0 +1,25 @@
+import unittest
+from calc import *
+
+class Test_Calc(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = calculator()
+
+    def tearDown(self):
+        pass
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(4, 5), 9)
+        self.assertEqual(self.calc.add(1, 4), 5)
+    
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(1, 5), 5)
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+        self.assertEqual(self.calc.multiply(5, 4), 20)
+
+    def test_division(self):
+        self.assertEqual(self.calc.division(5, 2), 2.5)
+        self.assertEqual(self.calc.division(5, -3), -(5 / 3))
+        self.assertRaises(ValueError, self.calc.division, 5, 0)
