0c3ac5ff564a2db8d4d3e631bb08013c395fb696
Jiahong He <jh3863@columbia.edu>
2018-10-25 03:24:00 -0400

    fixed bug and add subtract

--- calc.py
+++ calc.py
@@ -6,7 +6,10 @@ class calculator:
         self.name = "calculator"
 
     def add(self, a, b):
-        return np.add(a, b) + 1 # added a little bug here
+        return np.add(a, b)
+
+    def subtract(self, a, b):
+        return a - b
 
     def multiply(self, a, b):
         return np.multiply(a, b)
--- test_calc.py
+++ test_calc.py
@@ -13,6 +13,11 @@ class Test_Calc(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(4, 5), 9)
         self.assertEqual(self.calc.add(1, 4), 5)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(2, 3), -1)
+        self.assertEqual(self.calc.subtract(7, 5), 2)
+        self.assertEqual(self.calc.subtract(4, 4), 0)
     
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(1, 5), 5)
