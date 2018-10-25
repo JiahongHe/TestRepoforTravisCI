2ed7278bcbef411fdb185ba749c000df7d37392a
Jiahong He <jh3863@columbia.edu>
2018-10-25 03:11:39 -0400

    this should not build

--- calc.py
+++ calc.py
@@ -6,7 +6,7 @@ class calculator:
         self.name = "calculator"
 
     def add(self, a, b):
-        return np.add(a, b)
+        return np.add(a, b) + 1 # added a little bug here
 
     def multiply(self, a, b):
         return np.multiply(a, b)
